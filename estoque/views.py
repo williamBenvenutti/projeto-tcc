f886458e8e1cd36ef2986e7b386ba163cb9f1d9d
williamBenvenutti <benvenuttiwilliam@gmail.com>
2023-07-10 17:48:33 -0300

    alteracoes-finais

--- estoque/views.py
+++ estoque/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Estoque
 from produtos.models import Produto
 from django.db.models import Q
+from django.http import HttpResponse
+from django.contrib import messages
 
 def MostraEstoque(request):
-    produtos_vermelho = Estoque.objects.filter(Q(quantidade__lte = 3))
-    produtos_amarelo = Estoque.objects.filter(Q(quantidade__lt = 6) & Q(quantidade__gt = 3))
-    produtos_verde = Estoque.objects.filter(Q(quantidade__gte = 6))
+    produtos_vermelho = Estoque.objects.filter(quantidade = 0)
+    produtos_amarelo = Estoque.objects.filter(Q(quantidade__lt = 6) & Q(quantidade__gt = 0))
+    produtos_verde = Estoque.objects.filter(quantidade__gte = 6)
     produtos = Estoque.objects.filter(nome_produto__situacao=True)
     produtos_geral = Produto.objects.all()
 
@@ -24,11 +26,19 @@ def AdicionaEstoque(request, produto_id):
     if request.method == 'POST':
         quantidade = request.POST.get('quantidade')
 
-        produto = Estoque.objects.get(id=produto_id)
+        if not quantidade:
+            messages.error(request, 'Digite um valor válido!')
+            return redirect('mostra_estoque')
 
-        Estoque.objects.filter(id=produto_id).update(
-            quantidade = int(produto.quantidade) + int(quantidade)
-        )
+        try:
+            produto = get_object_or_404(Estoque, id=produto_id)
+            produto.quantidade += int(quantidade)
+            produto.save()
+        except (ValueError, Estoque.DoesNotExist):
+            messages.error(request, 'Produto não encontrado!')
+            return redirect('mostra_estoque')
+
+
         return redirect('mostra_estoque')
     else:
         return redirect('mostra_estoque')
